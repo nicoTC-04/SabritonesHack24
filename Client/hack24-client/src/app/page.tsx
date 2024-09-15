eed05a44f3725a8af3db37e3f2a6485ee6da1c24
GermanS04 <german.salasmar@gmail.com>
2024-09-14 18:22:55 -0600

    client component teacher pay card fixed

--- Client/hack24-client/src/app/page.tsx
+++ Client/hack24-client/src/app/page.tsx
@@ -1,11 +1,9 @@
-import TeacherPayCard from "@/components/explorar/TeacherPayCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <p>Landing</p>
-      <TeacherPayCard />
     </div>
   );
 }
