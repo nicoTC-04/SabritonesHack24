fe078cb24525bc911fb0ba3250e1ed58d74f412a
GermanS04 <german.salasmar@gmail.com>
2024-09-14 20:42:49 -0600

    client modal base component

--- Client/hack24-client/src/app/explorar/page.tsx
+++ Client/hack24-client/src/app/explorar/page.tsx
@@ -1,7 +1,23 @@
-'use client'
+"use client";
+
+import TeacherPayCard from "@/components/explorar/TeacherPayCard";
+import { useState } from "react";
+import ModalExplorarProfesor from "@/components/explorar/ModalExplorarProfesor";
+
+const description = "Ofrezco material al alumno para que aprenda de una manera eficiente y podamos tener un buen vinculo alumno maestro"
 
 export default function Explorar() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModalHandler = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
-    <div>Explorar</div>
-  )
+    <div>
+      Explorar
+      <TeacherPayCard name="Emilio Dominguez" image="aaa.jpg" rating={4.5} description={description} category="Matematicas" grade="Profesional" price={200} toggleModal={toggleModalHandler} />
+      {openModal && <ModalExplorarProfesor toggleModal={toggleModalHandler} />}
+    </div>
+  );
 }
