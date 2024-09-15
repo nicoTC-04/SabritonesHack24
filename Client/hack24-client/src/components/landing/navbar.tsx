89b4577a0c7d0395dcd91b165e3642e3d278aa5f
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-09-15 00:58:48 -0600

    login fixed

--- Client/hack24-client/src/components/landing/navbar.tsx
+++ Client/hack24-client/src/components/landing/navbar.tsx
@@ -1,23 +1,61 @@
+"use client";
 import Link from 'next/link';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import '@/styles/landing/navbar.css';
 
 const Navbar: React.FC = () => {
-  return (
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [userData, setUserData] = useState<any>(null);
+
+    useEffect(() => {
+        const storedUserData = localStorage.getItem('user');
+        if (storedUserData) {
+            setUserData(JSON.parse(storedUserData));
+        }
+    }, []);
+
+    const handleProfileClick = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('user');
+        setUserData(null);
+        setIsDropdownOpen(false);
+    };
+
+    return (
         <nav className="navbar">
             <div className="navItem_name">
-              <Link href="/" >Palestra</Link>
+                <Link href="/">Palestra</Link>
             </div>
             <div className="navItem">
-              <Link href="/explorar" className="navLink">Explorar</Link>
-              <Link href="/misclases" className="navLink">Mis Clases</Link>
+                <Link href="/explorar" className="navLink">Explorar</Link>
+                <Link href="/misclases" className="navLink">Mis Clases</Link>
             </div>
             <div className="navItem_profile">
-              <Link href="/" className="navLink">Mi Perfil</Link>
+                <button onClick={handleProfileClick} className="navLink">Mi Perfil</button>
+                {isDropdownOpen && (
+                    <div className="dropdown">
+                        {userData ? (
+                            <div>
+                                <p>Nombre: {userData.name}</p>
+                                <p>Región: {userData.region}</p>
+                                <p>Profesor: {userData.teacher}</p>
+                                <p>Zona Horaria: {userData.timezone}</p>
+                                <button onClick={handleLogout}>Cerrar Sesión</button>
+                            </div>
+                        ) : (
+                            <Link href="/login">
+                                <button>Iniciar Sesión</button>
+                            </Link>
+                        )}
+                    </div>
+                )}
             </div>
         </nav>
-      );
-
+    );
 };
 
 export default Navbar;
