3fe7fc964ea8161d7e3203884683c0b0ba54f6ec
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-09-14 18:50:46 -0600

    front landing and api fix

--- Client/hack24-client/src/app/page.tsx
+++ Client/hack24-client/src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+
+import React from "react";
+import SearchBar from "../components/landing/searchbar";
+import "./..//styles/landing/landing.css";
 
 export default function Home() {
   return (
     <div>
-      <p>Landing</p>
+      <div className="Banner">
+        <h1 className="header">Encuentra a tu nuevo Profesor</h1>
+      </div>
+      <div className="CenteredContainer">
+         <SearchBar />
+      </div>
+
+      <div>
+        <h2>Profesores Destacados</h2>
+      </div>
+
+      <div>
+        <h2>Categorías</h2>
+
+        <div>
+          <h3>Matematicas</h3>
+
+        </div>
+
+    </div>
     </div>
   );
 }
