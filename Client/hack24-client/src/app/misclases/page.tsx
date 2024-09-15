f3b14a6a6e60819340f36087e0b68880bb1fb160
Emi <emidmor@gmail.com>
2024-09-15 00:29:57 -0600

    [React]: Modal class and opening functions

--- Client/hack24-client/src/app/misclases/page.tsx
+++ Client/hack24-client/src/app/misclases/page.tsx
@@ -17,8 +17,8 @@ export default function MisClases() {
     <div className="myclasses-container">
       <div className="myclasses-left-container">
         <p className="myclasses-header">Próximas Clases</p>
-        <StudentClass name="German Salas" professor="Carlos Francisco Hernandez" fecha="Martes" hora="15:25pm" toggleModal={toggleModalHandler}/>
-        {openModal && <ModalClass toggleModal={toggleModalHandler} linkURL={"https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg"} nombre={"German Salas"} professor={"Carlos Francisco Hernandez"} fecha={"Martes"} hora={"15:25pm"} /> }
+        <StudentClass name="Matemáticas 101" professor="Carlos Francisco Hernandez" fecha="Martes" hora="15:25pm" toggleModal={toggleModalHandler}/>
+        {openModal && <ModalClass toggleModal={toggleModalHandler} linkURL={"https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg"} nombre={"Matemáticas 101"} professor={"Carlos Francisco Hernandez"} fecha={"Martes"} hora={"15:25pm"} /> }
       </div>
 
     </div>
--- Client/hack24-client/src/components/misclases/ModalClass.tsx
+++ Client/hack24-client/src/components/misclases/ModalClass.tsx
@@ -12,19 +12,24 @@ type ModalClassProps = {
 
 const ModalClass = ({ nombre, professor, fecha, hora, linkURL, toggleModal }: ModalClassProps) => {
     return (
-        <Modal width={80} height={90} modalToggle={toggleModal}>
-            <div className="modal-class-container">
-                <div className="modal-class-title">
-                    <p>{nombre}</p>
+        <Modal width={40} height={45} modalToggle={toggleModal}>
+            <div className="modal-class-general-container">
+                <div className="modal-class-container">
+                    <div className="modal-class-title">
+                        <p>{nombre}</p>
+                    </div>
+                    <div className="modal-class-professor">
+                        <p>{professor}</p>
+                    </div>
+                    <div className="modal-class-fecha">
+                        <p>{fecha} {hora}</p>
+                    </div>
+                    <div className="modal-class-link">
+                        <a href={linkURL} className="button">Link a tu clase!</a>
+                    </div>
                 </div>
-                <div className="modal-class-professor">
-                    <p>{professor}</p>
-                </div>
-                <div className="modal-class-fecha">
-                    <p>{fecha} {hora}</p>
-                </div>
-                <div className="modal-class-link">
-                    <a href={linkURL} className="button">Link a tu clase!</a>
+                <div className="modal-class-image">
+                    hi
                 </div>
             </div>
         </Modal>
